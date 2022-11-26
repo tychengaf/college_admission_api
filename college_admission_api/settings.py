d2830e5080ff353025df1de0c5d9e1a4df553b45
Tony Cheng <tychengaf@gmail.com>
2022-11-26 21:59:00 +0800

    add django_extensions settings

--- college_admission_api/settings.py
+++ college_admission_api/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    'django_extensions',
+]
+
+INSTALLED_APPS += [
     'admission',
 ]
 
@@ -126,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django_extensions
+SHELL_PLUS = "ipython"
